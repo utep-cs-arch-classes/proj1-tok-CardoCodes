80b76b6a9f8f59f4e2325f1cf89ef310430b20a8
CardoCodes <rmendez13@miners.utep.edu>
2021-02-11 14:04:24 -0700

    second upload of GUI.c

--- GUI.c
+++ GUI.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main(void){
-  char userIn[100];
-  printf(">:");
-  fgets(userIn,100,stdin);
-  printf(">(echo): %s", userIn);
-  return 0;
+
+  printf(">");
+  char c = getchar();
+  while(c != '\n'){
+    putchar(c);
+    c = getchar();
+  }
+  printf("\n");
 }
