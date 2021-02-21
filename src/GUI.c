6155eb0e81df209a5606f65c1f10b2a7f64050e0
CardoCodes <rmendez13@miner.utep.edu>
2021-02-20 20:23:16 -0700

    im commiting to master branch

---
+++ src/GUI.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+int main(void){
+
+
+
+  printf(">");
+
+  char c = getchar();
+
+  while(c != '\n'){
+
+    putchar(c);
+
+    c = getchar();
+
+  }
+
+  printf("\n");
+
+}
